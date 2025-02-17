cd7320865be6904730d8e3a37d8d89f5d1cc7b12
user <193957098+GauntletRunner2025@users.noreply.github.com>
2025-02-16 21:34:29 -0600

    add update() to derivers

--- Runtime/systems/Listener.cs
+++ Runtime/systems/Listener.cs
@@ -2,27 +2,32 @@ using Unity.Collections;
 using Unity.Entities;
 using UnityEngine;
 
-public abstract partial class Listener : ListenerBase {
+public abstract partial class Listener : ListenerBase
+{
 
     EntityQuery Query;
 
     public struct ListenerFlag : IComponentData { }
 
     public abstract bool OnEvent(EntityManager em, Entity e);
+    public abstract bool SystemUpdate(EntityManager em);
 
     //Abstract field the deriveer must implement
     public abstract ComponentType EventType { get; }
     public abstract ComponentType HandledFlagType { get; }
 
-    sealed protected override void OnCreate() {
+    sealed protected override void OnCreate()
+    {
         var e = EntityManager.CreateEntity();
-        EntityManager.AddComponentData(e, new EventTypeListenerPair {
+        EntityManager.AddComponentData(e, new EventTypeListenerPair
+        {
             EventType = EventType,
             ListenerType = typeof(ListenerFlag)
         });
 
 
-        var entityQueryDesc = new EntityQueryDesc {
+        var entityQueryDesc = new EntityQueryDesc
+        {
             All = new ComponentType[] { EventType, typeof(Listeners), typeof(LiveEvent) },
             None = new ComponentType[] { HandledFlagType },
         };
@@ -30,20 +35,27 @@ public abstract partial class Listener : ListenerBase {
         Query = GetEntityQuery(entityQueryDesc);
     }
 
-    protected override void OnUpdate() {
+    sealed protected override void OnUpdate()
+    {
+        SystemUpdate(EntityManager);
 
         using var entities = Query.ToEntityArray(Allocator.TempJob);
 
-        foreach (var entity in entities) {
-            if (!OnEvent(EntityManager, entity)) {
+        foreach (var entity in entities)
+        {
+            if (!OnEvent(EntityManager, entity))
+            {
                 continue;
-            } else {
+            }
+            else
+            {
                 //Here we add the handled flag to the event so it is no longer processed on subsequent frames
                 //This is because the event isn't destroyed right away if some listeners are waiting on something 
                 EntityManager.AddComponent(entity, HandledFlagType);
 
                 var buffer = EntityManager.GetBuffer<Listeners>(entity);
-                if (buffer.Length == 1) {
+                if (buffer.Length == 1)
+                {
                     //We were the last listener, so destroy the event
                     EntityManager.DestroyEntity(entity);
                     continue;
@@ -51,7 +63,8 @@ public abstract partial class Listener : ListenerBase {
 
                 //Some listeners other than us still remain
                 //Remove matching listener
-                for (int i = 0; i < buffer.Length; i++) {
+                for (int i = 0; i < buffer.Length; i++)
+                {
 
                     if (buffer[i].Value != typeof(ListenerFlag))
                         continue;
